f1a5342ce96c9e7b5b0449a5e83e0ec9e61b4192
DivyaSree Embar <de21364@corp.ojas-it.com>
2023-04-13 16:18:46 +0530

    coolcommit

---
+++ Practice/cool.js
@@ -0,0 +1,11 @@
+let arr=["str","strrr","fgsf"]
+arr.map((e,i)=>{
+    let b=e.split()
+    b.map((e,i)=>{
+        if(e.length%2==0)
+        {
+            console.log(e);
+        }
+    })
+   
+})
